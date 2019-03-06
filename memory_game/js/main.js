dd505c259442009f2d8d21c4f56f42dd8674d72a
pozithron <36094301+pozithron@users.noreply.github.com>
2019-03-06 14:01:03 -0500

    created game logic with JS

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!")
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+console.log("User Flipped "+cardOne);
+console.log("User Flipped "+cardTwo);
