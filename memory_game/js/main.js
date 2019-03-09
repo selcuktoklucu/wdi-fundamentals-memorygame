0401f205e8d665b43d029a10848d6eed46a66c99
pozithron <36094301+pozithron@users.noreply.github.com>
2019-03-09 01:01:35 -0500

    Created game logic, Arrays,checking Cards and If statement

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
 console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User Flipped "+cardOne);
-console.log("User Flipped "+cardTwo);
+
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay=[];
+var cardOne=cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped "+cardsInPlay);
+
+var cardTwo=cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped "+cardsInPlay);
+//instructions are not clear at project pbecause it shows both cardOne and cardTwo
+
+if(cardsInPlay.length == 2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found match!");
+
+	}
+	else{
+		alert("Sorry Try again!");
+	}
+}
