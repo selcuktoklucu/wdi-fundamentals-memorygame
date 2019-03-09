dd56994bb6da9a87ee44e6599e7a17a650d6cef3
pozithron <36094301+pozithron@users.noreply.github.com>
2019-03-09 17:31:10 -0500

    Created functions for flipping cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,21 +3,28 @@ console.log("Up and running!")
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay=[];
-var cardOne=cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped "+cardsInPlay);
-
-var cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+cardsInPlay);
-//instructions are not clear at project pbecause it shows both cardOne and cardTwo
-
-if(cardsInPlay.length == 2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found match!");
+var checkForMatch= function(){
+	if(cardsInPlay.length == 2){
+		if(cardsInPlay[0]===cardsInPlay[1]){
+			alert("You found match!");
 
 	}
 	else{
-		alert("Sorry Try again!");
+			alert("Sorry Try again!");
 	}
+} 
 }
+var flipCard = function (cardId){
+	console.log("the card was flipped over "+ cards[cardId]);
+	cardsInPlay.push(cardId);
+
+	//instructions are not clear at project pbecause it shows both cardOne and cardTwo
+
+	checkForMatch();
+}
+
+
+
+flipCard(0);
+flipCard(2);
+
